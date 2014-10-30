53561668a9b12f17cd48db14e2bee8f8dfd95fd3
root <root@it.ebh.com>
2014-10-30 14:53:23 +0800

    cccc

---
+++ tnx.go
@@ -0,0 +1,29 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+func WaitMany(a, b chan bool) {
+	for a != nil || b != nil {
+		select {
+		case <-a:
+			fmt.Println("a")
+			a = nil
+		case <-b:
+			fmt.Println("b")
+			b = nil
+		}
+	}
+}
+func main() {
+	a, b := make(chan bool), make(chan bool)
+	t0 := time.Now()
+	go func() {
+		close(a)
+		close(b)
+	}()
+	WaitMany(a, b)
+	fmt.Printf("waited %v for WaitMany\n", time.Since(t0))
+}
